dc74acfa9cc6c853d3c32b4c3d8dbcf02a74474a
Felix Livni <flivni@gmail.com>
2018-07-16 18:29:18 -0700

    Revert "Parsing I164 formatted Canadian numbers should result in CA territory."
    This reverts commit 184ad84560cbf3f9388f934ccb27c56e34aa44ad.

--- lib/global_phone/region.rb
+++ lib/global_phone/region.rb
@@ -30,11 +30,11 @@ module GlobalPhone
       territory_names.include?(name.to_s.upcase)
     end
 
-    def parse_national_string(string, preferred_territory = nil)
+    def parse_national_string(string)
       string = Number.normalize(string)
       if starts_with_country_code?(string)
         string = strip_country_code(string)
-        find_first_parsed_national_string_from_territories(string, preferred_territory)
+        find_first_parsed_national_string_from_territories(string)
       end
     end
 
@@ -55,13 +55,8 @@ module GlobalPhone
         string[country_code.length..-1]
       end
 
-      def find_first_parsed_national_string_from_territories(string, preferred_territory = nil)
-        # FELIX: First we look for a parsable preferred territory
-        national_string = Utils.map_detect(territories) { |territory| territory.name == preferred_territory and territory.parse_national_string(string) }
-
-        # If noparsable preferred territory, then we look for the first parsable territory
-        national_string ||= Utils.map_detect(territories) { |territory| territory.parse_national_string(string) }
-        national_string
+      def find_first_parsed_national_string_from_territories(string)
+        Utils.map_detect(territories) { |territory| territory.parse_national_string(string) }
       end
   end
 end
